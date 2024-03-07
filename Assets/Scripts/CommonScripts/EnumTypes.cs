43c70659f97c893796254aac03ef3424a8b57867
한인철 <InZury0923@gmail.com>
2024-03-07 19:10:57 +0900

    Asset 초기화

--- Assets/Scripts/CommonScripts/EnumTypes.cs
+++
@@ -1,9 +0,0 @@
-// Set Enum type Data
-// 열거형 데이터 타입 저장
-
-namespace EnumTypes
-{
-    public enum DirectionTypes { Left, Center, Right }
-
-    public enum CharacterState { Idle, Move, Jump, Attack, Die }
-}
